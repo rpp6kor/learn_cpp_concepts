5c1ec921cc92b21e612bd5a9e08a6ea1b4a109aa
Ravi Koppad <ravi.koppad@partner.bmw.de>
2021-03-20 13:49:44 +0100

    code to find factorial of a given number

---
+++ sample_cpp_programs/factorial.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdint>
+
+std::int32_t find_factorial(std::int32_t number)
+{
+    std::int32_t result{1};
+
+    while(number > 0)
+    {
+        result = result*number;
+        number = number-1;
+    }
+
+    return result;
+}
+
+int main()
+{
+    std::int32_t fact_num{};
+    std::cout << "Enter the number who factorial shall be calculated" << std::endl;
+
+    while(true)
+    {
+        std::cin >> fact_num;
+
+        if(fact_num < 0)
+        {
+            std::cout << "Entered number is invalid" << std::endl;
+        }
+        else
+        {
+            std::cout << "Factorial of a given number : " << find_factorial(fact_num) << std::endl;
+            break;
+        }
+
+    }
+}
